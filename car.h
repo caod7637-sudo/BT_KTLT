cfc87dea20202b27cbabb7cdee652e11c6b56638
nhtqnh <quynhdn915981@ut.edu.vn>
2026-07-08 11:49:45 +0700

    Ver2 car.h

--- car.h
+++ car.h
@@ -8,6 +8,7 @@ public:
     // Hàm khởi tạo nhận đầy đủ 4 tham số truyền cho Entity, kết hợp nạp giá trị mặc định
     Car(float x, float y, float width, float height);
 
+    // ĐÁP ỨNG ĐỦ DANH SÁCH API THEO YÊU CẦU CỦA FILE TÀI LIỆU
     void setLane(int laneIndex);  // Đặt thực thể vào làn đường chỉ định (1, 2 hoặc 3) [cite: 23]
     int getLane() const;          // Lấy làn đường hiện tại của đối tượng [cite: 24]
     void setSpeed(int speed);     // Định cấu hình vận tốc rơi/di chuyển của xe cản địa [cite: 25]
